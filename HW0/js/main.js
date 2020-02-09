78a4de038409385b6824dbd1b7226a9e8cf61bf9
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-09 17:32:01 -0500

    Update main.js

--- HW0/js/main.js
+++ HW0/js/main.js
@@ -14,14 +14,14 @@ window.onload = function() {
     
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image( 'cat', 'cat_idle/phaser.png' );
     }
     
     var bouncy;
     
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'cat' );
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
         bouncy.anchor.setTo( 0.5, 0.5 );
