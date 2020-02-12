a707c33cd0a7daadae0fd6f3b57dc822f4967b76
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-12 14:45:37 -0500

    Update main.js

--- Digital Assignment 1/js/main.js
+++ Digital Assignment 1/js/main.js
@@ -80,7 +80,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 
 function preload () {
 
-    game.load.spritesheet('tank', 'assets/New Pixel (1).png', 64, 95, 5);// 'assets/tanks/tanks.json');
+    game.load.spritesheet('tank', 'assets/New Piskel (1).png', 64, 95, 5);// 'assets/tanks/tanks.json');
     game.load.atlas('enemy', 'assets/tanks/enemy-tanks.png', 'assets/tanks/tanks.json');
     game.load.image('logo', 'assets/tanks/logo.png');
     game.load.image('bullet', 'assets/tanks/bullet.png');
