6982a0a0c41204354f553100b171c6f09dab0c49
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-12 21:08:43 -0500

    Update main.js

--- Digital Assignment 1/js/main.js
+++ Digital Assignment 1/js/main.js
@@ -8,7 +8,7 @@ function preload() {
     game.load.spritesheet('ship', 'assets/walk.png', 32,32 );
     game.load.spritesheet('shoot', 'assets/shoot.png', 320, 64); 
     game.load.spritesheet('kaboom', 'assets/invaders/explode.png', 128, 128);
-    game.load.image('starfield', 'assets/invaders/starfield.png');
+   // game.load.image('starfield', 'assets/bg.png');
     game.load.image('background', 'assets/bg.png');
 
 }
@@ -35,7 +35,7 @@ function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //  The scrolling starfield background
-    starfield = game.add.tileSprite(0, 0, 800, 600, 'starfield');
+    //starfield = game.add.tileSprite(0, 0, 800, 600, 'starfield');
 
     //  Our bullet group
     bullets = game.add.group();
