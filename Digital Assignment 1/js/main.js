dd8a5e36d2c852c60f6b7b2671b46f5bf6378ec7
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-12 23:16:30 -0500

    Update main.js

--- Digital Assignment 1/js/main.js
+++ Digital Assignment 1/js/main.js
@@ -260,30 +260,8 @@ function enemyHitsPlayer (player,bullet) {
     
     bullet.kill();
 
-    live = lives.getFirstAlive();
-
-    if (live)
-    {
-        live.kill();
-    }
-
-    //  And create an explosion :)
-    var explosion = explosions.getFirstExists(false);
-    explosion.reset(player.body.x, player.body.y);
-    explosion.play('kaboom', 30, false, true);
-
-    // When the player dies
-    if (lives.countLiving() < 1)
-    {
-        player.kill();
-        enemyBullets.callAll('kill');
-
-        stateText.text=" GAME OVER \n Click to restart";
-        stateText.visible = true;
-
-        //the "click to restart" handler
-        game.input.onTap.addOnce(restart,this);
-    }
+    score += 2;
+    scoreText.text = scoreString + score;
 
 }
 
