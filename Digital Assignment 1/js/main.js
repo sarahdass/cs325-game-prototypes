f4f26a026ecf0370ead3c88945c25b4360fff72e
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-12 21:52:54 -0500

    Update main.js

--- Digital Assignment 1/js/main.js
+++ Digital Assignment 1/js/main.js
@@ -8,8 +8,8 @@ function preload() {
     game.load.spritesheet('ship', 'assets/walk.png', 32,32 );
     game.load.spritesheet('shoot', 'assets/shoot.png', 320, 64); 
     game.load.spritesheet('kaboom', 'assets/explo.png', 694 , 83, 11);
-    game.load.image('starfield', 'assets/bg.png');
-    game.load.image('background', 'assets/bg.png');
+    game.load.image('starfield', 'assets/invaders/starfield.png');
+    game.load.image('background', 'assets/background2/bg.png');
 
 }
 
