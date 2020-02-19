7a0a9be64e5fa089578c8dde2d8e2ae0d439954b
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-19 14:43:33 -0500

    Add files via upload

---
+++ Digital Assignment 2/assets/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="tiles" tilewidth="32" tileheight="32" tilecount="12" columns="4">
+ <image source="tileset.png" width="128" height="104"/>
+</tileset>
