1b016e914c9439cf740e5f46b4ebc07393bbaa34
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-19 15:23:53 -0500

    Update main.js

--- Digital Assignment 2/js/main.js
+++ Digital Assignment 2/js/main.js
@@ -1,4 +1,4 @@
-pvar game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
