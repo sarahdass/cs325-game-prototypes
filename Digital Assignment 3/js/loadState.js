ca60c88d8767dfe833eb845f832f3f687df39e19
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-24 13:19:44 -0500

    Update loadState.js

--- Digital Assignment 3/js/loadState.js
+++ Digital Assignment 3/js/loadState.js
@@ -1,1 +1,10 @@
+var loadState{
+  preload: function(){
+    var loadlabel = game.add.text(80, 150, 'loading...', {font: '30px Ariel', fill: '#ffffff'});
+    game.load.image('logo', 'assets/phaser_logo.png');
+  },
+  create: function(){
+    game.state.start.('menu');
+  }
+};
 
