6e9b412392647b09b1e55af33a0fcf1015e42147
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-24 13:49:17 -0500

    Update endState.js

--- Digital Assignment 3/js/endState.js
+++ Digital Assignment 3/js/endState.js
@@ -1,9 +1,9 @@
 
 var endState = {
   create: function(){
-    var name = game.add.text(80, 80, 'End of game', {font: '50px Ariel', fill: '#ffffff' });
-    var start = game.add.text(25, 25, 'Press S to start', {font: '25px Ariel', fill: '#ffffff' });
-    var skey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    var name = game.add.text(80, 80, 'End of game', {font: '50px Arial', fill: '#ffffff' });
+    var start = game.add.text(25, 25, 'Press S to start', {font: '25px Arial', fill: '#ffffff' });
+    var skey = game.input.keyboard.addKey(Phaser.Keyboard.S);
     skey.isDown.addOnce(this.start, this);
   },
   start: function() {
