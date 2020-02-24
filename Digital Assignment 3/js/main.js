9314377eae3622121ce901ae98f2d43aa465dc57
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-24 14:32:26 -0500

    Update main.js

--- Digital Assignment 3/js/main.js
+++ Digital Assignment 3/js/main.js
@@ -49,25 +49,25 @@ function make_main_game_state( game )
 
 function endState(game) {
 
-	create: function () {
+	function create() {
         var name = game.add.text(80, 80, 'End of game', {font: '50px Arial', fill: '#ffffff' });
         var start = game.add.text(25, 25, 'Press S to go to menu, {font: '25px Arial', fill: '#ffffff' });
         var skey = game.input.keyboard.addKey(Phaser.Keyboard.S);
         skey.isDown.addOnce(this.startGame, this);
     },
 
-	startGame: function (pointer) {
+	function startGame(pointer) {
 		this.state.start('menu');
 	}
 };
 function menuState(game) {
-  create: function(){
+  function create(){
     var name = game.add.text(80, 80, 'Game', { font: '84px Arial', fill: '#fff' });
     var start = game.add.text(80, game.world.height-80, 'Press S to start', { font: '25px Arial', fill: '#ffffff' });
     var skey = game.input.keyboard.addKey(Phaser.Keyboard.S);
     skey.isDown.addOnce(this.start, this);
   },
-  start: function() {
+  function start(){
     game.state.start('main');
   }
 };
