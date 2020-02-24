a08bc3cf295f136d2af76639af37f6ec62709486
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-24 13:52:08 -0500

    Update menuState.js

--- Digital Assignment 3/js/menuState.js
+++ Digital Assignment 3/js/menuState.js
@@ -1,8 +1,8 @@
 var menuState = {
   create: function(){
     var name = game.add.text(80, 80, 'Game', {font: '50px Arial', fill: '#ffffff' });
-    var start = game.add.text(25, 25, 'Press S to start', {font: '25px Arial', fill: '#ffffff' });
-    var skey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    var start = game.add.text(80, game.world.height-80, 'Press S to start', {font: '25px Arial', fill: '#ffffff' });
+    var skey = game.input.keyboard.addKey(Phaser.Keyboard.S);
     skey.isDown.addOnce(this.start, this);
   },
   start: function() {
