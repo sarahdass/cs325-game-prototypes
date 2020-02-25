6d7fdea5abaed3d2bdfdf9c7c2942e0e116582b6
sarahdass <60455613+sarahdass@users.noreply.github.com>
2020-02-25 13:27:03 -0500

    Update Game.js

--- Digital Assignment 3/js/Game.js
+++ Digital Assignment 3/js/Game.js
@@ -68,7 +68,7 @@ BasicGame.Game.prototype = {
         // When you click on the sprite, you go back to the MainMenu.
         //this.bouncy.inputEnabled = true;
         //this.bouncy.events.onInputDown.add( function() { this.quitGame(); }, this );
-        this.add( function() {this.createcats(); } this);
+        this.add( function() {this.createcats(); }, this);
         this.physics.arcade.sortDirection = Phaser.Physics.Arcade.SORT_NONE;
         this.stage.backgroundColor = '#BFF068';
         this.map = this.game.add.tilemap('kitchen');
